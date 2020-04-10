ad57d39c8d6aad30b49de4a008f8550702961ad7
archfan <33993466+archfan7411@users.noreply.github.com>
2020-04-10 08:30:04 -0400

    goodbye shadow realm :(

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@
 
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
-dofile(modpath.."/realm.lua")
 dofile(modpath.."/items.lua")
 dofile(modpath.."/mobs.lua")
 dofile(modpath.."/nodes.lua")
