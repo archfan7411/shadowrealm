bcbdc98ec8f63fe9c325664d658a57648bf172db
Deathwing777 <160651137+Deathwing777@users.noreply.github.com>
2024-02-20 11:23:20 -0800

    Add files via upload
    This fixes Issue 1 "This mod should be Nether-aware. #1" with the upstream codebase.

--- realm.lua
+++ realm.lua
@@ -2,6 +2,16 @@
 
 realm = {}
 
+-- If the nether mod is installed, inquire about nether player status.
+local is_player_in_nether
+if NETHER == nil then
+	is_player_in_nether = function (player)
+		return false
+	end
+else
+	is_player_in_nether = NETHER['is_player_in_nether']
+end
+
 local modstorage = minetest.get_mod_storage()
 
 realm.is_gargantuan_defeated = modstorage:get_int("dead") == 1
@@ -20,9 +30,11 @@ minetest.register_globalstep(function(dtime)
 		if not (timer >= 1) then return end
 		timer = 0
 		for _, player in pairs(minetest.get_connected_players()) do
-			local pos = player:get_pos()
-			if pos.y < -200 then
-				player:set_hp(player:get_hp()-1)
+			if not is_player_in_nether(player) then
+				local pos = player:get_pos()
+				if ( pos.y < -200 ) then
+					player:set_hp(player:get_hp()-1)
+				end
 			end
 		end
 	end
